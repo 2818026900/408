906e9c226a029d7fca12b9494f53f4bb4986f5d5
2818026900 <58018630+2818026900@users.noreply.github.com>
2020-10-29 14:43:03 +0800

    Update LinkList.c

--- 数据结构/线性表/LinkList.c
+++ 数据结构/线性表/LinkList.c
@@ -86,7 +86,7 @@ LinkList TailInsert() {
 
 /*
 	按位查找
-	@peram i 所要查找的值所处位值
+	@param i 所要查找的值所处位值
 	GetElem()
 */
 link* GetElem(LinkList head, int i) {
@@ -107,6 +107,85 @@ link* GetElem(LinkList head, int i) {
 }
 
 /*
+	按值查找
+	@param ElemType e 所要查找元素
+	return int i 所查找元素位置
+	LocateElem()
+*/
+int LocateElem(LinkList head,ElemType e) {
+	int i;
+	head = head->next;
+	for (i = 0; head != NULL; i++) {
+		if (head->data == e) {
+			return i + 1;
+		}
+		head = head->next;//移动指针位置
+	}
+	return -1;
+}
+
+/*
+	插入节点（前插）
+	@param ElemType e 所要插入的值
+	@param int i 所要插入节点的当前位置
+	return head 返回插入成功后的头指针
+	BeforeInstert()
+*/
+LinkList BefortInstert(LinkList head,ElemType e ,int i) {
+	//找到插入节点的前驱位置p,L为新节点
+	link* p,*L;
+	p = GetElem(head,i-1);
+	//插入节点，移动元素
+	L = (link*)malloc(sizeof(link));
+	L->data = e;
+	L->next = p->next;
+	p->next = L;
+	return head;
+}
+
+/*
+	插入节点（后插）
+	@param ElemType e 所要插入的值
+	@param int i 所要插入节点的当前位置
+	return head 返回插入成功后的头指针
+	BeforeInstert()
+*/
+LinkList AfterInstert(LinkList head, ElemType e, int i) {
+	link* p, * L;//找到插入节点的当前位置p,L为新节点
+	p = GetElem(head , i);
+	//插入节点移动位置
+	L = (link*)malloc(sizeof(link));
+	L->data = e;
+	L->next = p->next;
+	p->next = L;
+	return head;
+}
+
+/*
+	删除指定位置节点
+	@param int i 待删除元素的位置
+	DeleteElem();
+*/
+LinkList DeleteElem(LinkList head, int i) {
+	link* p, * q;//找到待删除节点的前驱p,当前节点q
+	p = GetElem(head , i-1);
+	q = p->next;
+	p->next = q->next;
+	free(q);
+	 
+	/*
+		free(q)时出现断点错误是因为在BeforeInsert/AfterInsert中申请内存空间时使用
+			(ElemType*)malloc(ElemType);
+		这句话只为指针申请了空间而没有为结构体申请空间，所以释放后空间不足以放下结构题引发断点
+		正确写法：
+			(link*)malloc(link);
+		参考文章URL:https://blog.csdn.net/linzi_1988/article/details/79564440?utm_medium=distribute.pc_relevant_ask_down.none-task-blog-baidujs-1.nonecase&depth_1-utm_source=distribute.pc_relevant_ask_down.none-task-blog-baidujs-1.nonecase
+	*/
+
+	return head;
+}
+
+/*
 	遍历链表
 */
 void PrintList(LinkList head) {
@@ -118,8 +197,24 @@ void PrintList(LinkList head) {
 	printf("\n");
 }
 
+
+/*
+	求表长（不带头节点）
+*/
+int ListLength(LinkList head) {
+	int l=0;
+	while (head->next)
+	{
+		l++;
+		head = head->next;//切记移动头指针
+	}
+	return l;
+}
+
+
 int main() {
 	link* head;
+	ElemType e;
 	head = HeadInsert();
 	printf("头插法：\n");
 	PrintList(head);
@@ -127,6 +222,15 @@ int main() {
 	head = TailInsert();
 	printf("尾插法：\n");
 	PrintList(head);
-	printf("第2个值是%d\n",*GetElem(head,2));
+	printf("请输入要查找的值：");
+	scanf("%d", &e);
+	printf("\n值%d的位置是%d\n", e,LocateElem(head,e));
+	head = BefortInstert(head,10,2);
+	PrintList(head);
+	head = AfterInstert(head, 50, 2);
+	PrintList(head);
+	head = DeleteElem(head,3);
+	PrintList(head);
+	printf("当前表长为%d\n",ListLength(head));
 	return 0;
 }
